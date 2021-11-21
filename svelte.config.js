5df57c8b46b7fda2f7bf14ed02ea3ad6fd0a8da9
jacob-hvsb <jacob@hvsb.app>
2021-11-21 08:48:30 +0000

    prepare for gitpod

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,12 @@ const config = {
 		adapter: vercel(),
 		target: '#svelte',
 		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443 : 24678,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
+			},
 			ssr: {
 				// noExternal:  ["d3", "d3-geo", "d3-scale", "d3-array", "d3-interpolate", "d3-format", "d3-time", "d3-time-format"]
 				noExternal: ["d3"]
