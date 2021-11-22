6f890417fd46f927a3c5ec53dd1da328ecd6be55
jacob-hvsb <jacob@hvsb.app>
2021-11-22 09:29:22 +0000

    add sveltefirets

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script context="module" lang="ts">
+  import { initFirebase } from 'sveltefirets';
+  import { firebaseConfig } from '$lib/firebaseConfig';
+  import type { Load } from '@sveltejs/kit';
+  export const load: Load = async () => {
+    initFirebase(firebaseConfig);
+    return {};
+  };
+</script>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 			},
 			ssr: {
 				// noExternal:  ["d3", "d3-geo", "d3-scale", "d3-array", "d3-interpolate", "d3-format", "d3-time", "d3-time-format"]
-				noExternal: ["d3"]
+				noExternal: ['d3', 'sveltefirets']
 			}
 		}
 	}
